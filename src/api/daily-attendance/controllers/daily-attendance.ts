068c93c3a71fb4bdf27b87ab0206f50447e17ed7
gautamxyzstudio <168428105+gautamxyzstudio@users.noreply.github.com>
2024-06-05 13:10:55 +0530

    add

---
+++ src/api/daily-attendance/controllers/daily-attendance.ts
@@ -0,0 +1,7 @@
+/**
+ * daily-attendance controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::daily-attendance.daily-attendance');
---
+++ src/api/daily-attendance/routes/daily-attendance.ts
@@ -0,0 +1,7 @@
+/**
+ * daily-attendance router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::daily-attendance.daily-attendance');

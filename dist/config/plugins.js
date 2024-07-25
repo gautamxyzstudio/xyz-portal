e4ea7e70196dbdc7af9c23a7eae9b0e9ef06bafe
gautamxyzstudio <168428105+gautamxyzstudio@users.noreply.github.com>
2024-07-25 14:08:14 +0530

    image path set

--- dist/config/plugins.js
+++ dist/config/plugins.js
@@ -1,8 +1,14 @@
 module.exports = ({ env }) => ({
     upload: {
-        provider: 'local',
-        providerOptions: {
-            sizeLimit: 1000000,
+      provider: 'local',
+      providerOptions: {
+        sizeLimit: 1000000, // optional, default is 1MB
+      },
+      actionOptions: {
+        upload: {
+          folder: env('UPLOAD_PATH'),
         },
+      },
     },
-});
+  });
+  

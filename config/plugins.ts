82e624ba975d84d01ee77c79d15c62af42b669b3
gautamxyzstudio <168428105+gautamxyzstudio@users.noreply.github.com>
2024-07-25 12:19:45 +0530

    automatic assets remove fix

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,9 +1,14 @@
 module.exports = ({ env }) => ({
-    upload: {
+  upload: {
+    config: {
       provider: 'local',
       providerOptions: {
-        sizeLimit: 1000000,
+        sizeLimit: 1000000, // Maximum file size in bytes
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-  });
-  
+  },
+});

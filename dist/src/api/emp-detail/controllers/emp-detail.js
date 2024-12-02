cb6c992f6cb8db271ec402bd2f8c58a439babb5c
gautamxyzstudio <168428105+gautamxyzstudio@users.noreply.github.com>
2024-12-02 16:47:31 +0530

    fix code

--- dist/src/api/emp-detail/controllers/emp-detail.js
+++ dist/src/api/emp-detail/controllers/emp-detail.js
@@ -1,40 +1,26 @@
-"use strict";
-/**
- * daily-attendance controller
- */
+'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * emp-detail controller
+ */
 const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::daily-attendance.daily-attendance', ({ strapi }) => ({
+exports.default = strapi_1.factories.createCoreController('api::emp-detail.emp-detail', ({ strapi }) => ({
     async find(ctx) {
         ctx.query.populate = {
-            emp_details: {
-                populate: {
-                    Photo: true
-                }
-            }
+            Photo: true,
+            leave_status: true,
+            daily_attendances: true, // Populate the one-to-many daily_attendances relation
         };
         const { data, meta } = await super.find(ctx);
         return { data, meta };
     },
     async findOne(ctx) {
         ctx.query.populate = {
-            emp_details: {
-                populate: {
-                    Photo: true
-                }
-            }
+            Photo: true,
+            leave_status: true,
+            daily_attendances: true, // Populate the one-to-many daily_attendances relation
         };
         const { data, meta } = await super.findOne(ctx);
         return { data, meta };
     },
-    async create(ctx) {
-        const response = await super.create(ctx);
-        const entity = await strapi.entityService.findOne('api::daily-attendance.daily-attendance', response.data.id, { populate: { emp_details: { populate: { Photo: true } } } });
-        return { data: entity };
-    },
-    async update(ctx) {
-        const response = await super.update(ctx);
-        const entity = await strapi.entityService.findOne('api::daily-attendance.daily-attendance', response.data.id, { populate: { emp_details: { populate: { Photo: true } } } });
-        return { data: entity };
-    }
 }));
--- src/api/emp-detail/controllers/emp-detail.ts
+++ src/api/emp-detail/controllers/emp-detail.ts
@@ -1,17 +1,17 @@
+'use strict';
+
 /**
- * daily-attendance controller
+ * emp-detail controller
  */
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::daily-attendance.daily-attendance', ({ strapi }) => ({
+export default factories.createCoreController('api::emp-detail.emp-detail', ({ strapi }) => ({
   async find(ctx) {
     ctx.query.populate = {
-      emp_details: {
-        populate: {
-          Photo: true
-        }
-      }
+      Photo: true, // Populate the Photo media field
+      leave_status: true, // Populate the one-to-one leave_status relation
+      daily_attendances: true, // Populate the one-to-many daily_attendances relation
     };
 
     const { data, meta } = await super.find(ctx);
@@ -20,34 +20,12 @@ export default factories.createCoreController('api::daily-attendance.daily-atten
 
   async findOne(ctx) {
     ctx.query.populate = {
-      emp_details: {
-        populate: {
-          Photo: true
-        }
-      }
+      Photo: true, // Populate the Photo media field
+      leave_status: true, // Populate the one-to-one leave_status relation
+      daily_attendances: true, // Populate the one-to-many daily_attendances relation
     };
 
     const { data, meta } = await super.findOne(ctx);
     return { data, meta };
   },
-
-  async create(ctx) {
-    const response = await super.create(ctx);
-    const entity = await strapi.entityService.findOne(
-      'api::daily-attendance.daily-attendance',
-      response.data.id,
-      { populate: { emp_details: { populate: { Photo: true } } } }
-    );
-    return { data: entity };
-  },
-
-  async update(ctx) {
-    const response = await super.update(ctx);
-    const entity = await strapi.entityService.findOne(
-      'api::daily-attendance.daily-attendance',
-      response.data.id,
-      { populate: { emp_details: { populate: { Photo: true } } } }
-    );
-    return { data: entity };
-  }
 }));
